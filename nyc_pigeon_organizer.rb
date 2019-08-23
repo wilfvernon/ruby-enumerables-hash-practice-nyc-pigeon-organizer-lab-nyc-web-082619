94dbf57cf88c315f6c41ab64ea63276b568a9bb8
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-23 05:54:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted = Hash.new
+  name_array = Array.new
+  catalogue_array = Array.new
+  result_array = Array.new
+  data.each do |key, value|
+    value.each do |nest, name|
+      name_array.push(name)
+    end 
+  end
+  name_array.flatten!
+  name_array.uniq!
+  name_array.sort!
+  name_array.each do |bird|
+    sorted[bird] = {}
+  end
+  data.each_key do |symbol|
+    catalogue_array.push(symbol)
+  end
+  sorted.each_key do |bird|
+   sorted[bird] = Hash[catalogue_array.collect { |item| [item, {}] } ]
+  end
+  
+  sorted.each_key do |bird|
+    sorted[bird].each_key do
+      data.each do|key, value|
+        value.each do |nest, names|
+          names.each do |name|
+            binding.pry
+            if name == bird
+            sorted[bird][key] = 
+            end
+          end
+        end  
+      end  
+    end
+  end    
+  binding.pry
 end
